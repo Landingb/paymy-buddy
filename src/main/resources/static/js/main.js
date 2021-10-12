eeb8da12ade651cd5eabbbc70302168a249725ed
Lunaesky <landing.badji@outlook.com>
2021-10-12 15:07:30 +0200

    update thymeleaf

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,17 @@
+//Sort a form input select by text:
+$(document).ready(function() {
+
+	let optionsList = $('#userDestination option');
+	let selected = $("#userDestination").val();
+
+	optionsList.sort(function(a,b){
+	    if (a.text > b.text) return 1;
+	    if (a.text < b.text) return -1;
+	    return 0
+
+	});
+
+	$('#userDestination').html(optionsList);
+	$("#userDestination").val(selected);
+
+});
